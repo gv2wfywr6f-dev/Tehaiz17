d6ebd7645f05ecc0d97b2e219391889bcbc57a6a
Tahreen <gv2wfywr6f@privaterelay.appleid.com>
2026-07-19 22:56:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,10 +5,9 @@ const opening = document.getElementById("opening");
 const music = document.getElementById("bgMusic");
 
 
-openBtn.addEventListener("click", function(){
+openBtn.addEventListener("click", () => {
 
 opening.classList.add("hide");
-
 
 if(music){
 music.play();
@@ -18,33 +17,32 @@ music.play();
 
 
 
-
 // Countdown
 
 const weddingDate = new Date("December 4, 2026 19:00:00").getTime();
 
 
-function countdown(){
+function updateCountdown(){
 
 const now = new Date().getTime();
 
 const distance = weddingDate - now;
 
 
-const days = Math.floor(distance/(1000*60*60*24));
+const days = Math.floor(distance / (1000*60*60*24));
 
 const hours = Math.floor(
-(distance%(1000*60*60*24))/(1000*60*60)
+(distance % (1000*60*60*24)) / (1000*60*60)
 );
 
 
 const minutes = Math.floor(
-(distance%(1000*60*60))/(1000*60)
+(distance % (1000*60*60)) / (1000*60)
 );
 
 
 const seconds = Math.floor(
-(distance%(1000*60))/1000
+(distance % (1000*60)) / 1000
 );
 
 
@@ -57,10 +55,9 @@ document.getElementById("minutes").innerHTML = minutes;
 
 document.getElementById("seconds").innerHTML = seconds;
 
-
 }
 
 
-setInterval(countdown,1000);
+setInterval(updateCountdown,1000);
 
-countdown();
+updateCountdown();
